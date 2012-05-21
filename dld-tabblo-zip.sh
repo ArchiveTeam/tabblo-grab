6aad09ce301b09ce95f407a5d49daf34e8d44a55
Alard <alard@example.com>
2012-05-21 12:58:55 +0200

    Concurrent downloading of ranges.

--- dld-tabblo-zip.sh
+++ dld-tabblo-zip.sh
@@ -4,23 +4,31 @@
 # This script downloads Tabblo ZIP files and uploads to s3.us.archive.org.
 #
 # Usage:
-#  ./dld-tabblo-zip.sh $RANGE
-# where $RANGE is a number.
+#  ./dld-tabblo-zip.sh $RANGE [$PART]
+# where $RANGE is a number and $PART, optional, is [0-9].
 #
 # Example:
 #  ./dld-tabblo-zip.sh 12
 # will download and upload Tabblos 12000 to 12999.
+#  ./dld-tabblo-zip.sh 12 2
+# will download and upload Tabblos 12200 to 12299.
 #
 # Notes:
 # - You can kill and restart the script at any time, it will resume after
 #   the last uploaded Tabblo.
+# - touch STOP to stop gracefully.
 # - The Tabblo ZIP function is somewhat unstable; sometimes the download
 #   stop in the middle of the ZIP file. The script will check the ZIP
 #   and will redownload it if necessary.
 # - It's okay to run more than one script at a time in the same directory,
 #   as long as each script downloads a different range.
+# - To speed up downloading, consider running more than one client for a
+#   range, each downloading one part. E.g.,
+#     for PART in $( seq 0 9 ) ; do
+#       ./dld-tabblo-zip.sh $RANGE $PART &
+#     done
 #
-# Version 1.
+# Version 2.
 #
 
 USERNAME=archiveteam
@@ -31,6 +39,7 @@ S3_SECRET=47BbERXrOULLrzXX
 
 DATA=data
 RANGE=$1
+PART=$2
 
 if [[ -z $RANGE ]] || [[ ! $RANGE =~ ^[0-9]+$ ]]
 then
@@ -38,12 +47,38 @@ then
   exit
 fi
 
+if [[ ! -z $PART ]] && [[ ! $PART =~ ^[0-9]$ ]]
+then
+  echo "Invalid part number."
+  exit
+fi
+
 echo -n "Logging in... "
 LOGIN_COOKIE=$( curl -si -d "username=${USERNAME}&password=${PASSWORD}&remember_me=on" "http://www.tabblo.com/studio/login/" | grep -ohE "tabblosesh=[^;]+" )
 echo $LOGIN_COOKIE
 
-for TABBLO_ID in $( seq ${RANGE}000 ${RANGE}999 )
+if [[ -z $PART ]]
+then
+  FROM_ID=${RANGE}000
+  TO_ID=${RANGE}999
+else
+  FROM_ID=${RANGE}${PART}00
+  TO_ID=${RANGE}${PART}99
+fi
+
+initial_stop_mtime='0'
+if [ -f STOP ]
+then
+  initial_stop_mtime=$( ./filemtime-helper.sh STOP )
+fi
+
+for TABBLO_ID in $( seq ${FROM_ID} ${TO_ID} )
 do
+  if [[ -f STOP ]] && [[ $( ./filemtime-helper.sh STOP ) -le $initial_stop_mtime ]]
+  then
+    exit
+  fi
+
   TABBLO_ID8=$( printf "%08d" ${TABBLO_ID} )
   TABBLO_DIR=${TABBLO_ID8:0:2}/${TABBLO_ID8:0:5}
   mkdir -p "${DATA}/tabblo/${TABBLO_DIR}"
@@ -101,7 +136,7 @@ do
           sleep 60
         done
 
-        echo " - Uploaded."
+        echo " - Uploaded Tabblo ${TABBLO_ID}."
         mv "${DATA}/tmp.$$.zip" "${DATA}/tabblo/${TABBLO_DIR}/tabblo-${TABBLO_ID8}.zip"
         break
       else
